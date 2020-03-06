dc8bdb9564d5197dd45272183b968e3f368740d7
KoreanCow <tyunnon1257@naver.com>
2020-03-06 20:20:25 +0900

    2020-03-06

---
+++ todo-app/src/components/TodoList.js
@@ -0,0 +1,18 @@
+// todos 배열을 props로 받아 온 후, 이를 배열 내장 함수 map을 사용해서
+// 여러 개의 TodoListItem 컴포넌트로 반환 하여 보여줍니다.
+
+import React from 'react';
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+  return (
+    <div className="TodoList">
+      {todos.map(todo => (
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle}/>
+      ))}
+    </div>
+  );
+};
+
+export default TodoList;
